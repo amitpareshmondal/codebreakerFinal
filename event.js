f4f5ef804666e196e91a46045064f31cf19a15ac
Amit <mandalamit325@gmail.com>
2021-11-08 22:03:54 +0530

    First commit

---
+++ event.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    animateIn:  'animate__pulse',
+    animateOut:  'animate__pulse',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
